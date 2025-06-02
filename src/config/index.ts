d88746abf8ace124e77acef15fde45b31b8caec4
Mohamed sayed <mhmdsayed90031@gmail.com>
2025-06-02 20:44:15 +0300

    add api url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   timeout: 10000,
 });
+
 
 export default axiosInstance;
